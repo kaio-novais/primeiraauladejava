5422f34de8e07621648e7a9570d26fe2378eee52
kaio reisson <91042007+kaio-novais@users.noreply.github.com>
2021-12-21 11:40:46 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,9 @@
+var x = "nome"
+var y = "idade"
+
+console.log("o aluno "+x+" tem "+y+" anos de experiencia")
+
+var x = "kaio"
+var y = "20"
+
+console.log("O aluno "+x+" possue "+y+" Anos de idade")
